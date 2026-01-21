28ab1016d8c51228745831f40e737c5ce7b751d7
ayushmko73 <164129893+ayushmko73@users.noreply.github.com>
2026-01-21 11:26:10 +0530

    chore(autonomous): sync src/main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+import React from 'react';\nimport ReactDOM from 'react-dom/client';\nimport App from './App';\nimport './index.css';\n\nReactDOM.createRoot(document.getElementById('root')!).render(\n  <React.StrictMode>\n    <App />\n  </React.StrictMode>\n);
